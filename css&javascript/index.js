4dd6752cd88ca2c0275c584081c5064a82bb54f8
Mmezy <jessiemgbachi@gmail.com>
2025-07-02 10:27:42 +0100

    Add files via upload

---
+++ css&javascript/index.js
@@ -0,0 +1,61 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+/*=============== CHANGE BG Header ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+bgHeader()
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true,  Aniamtion repeat
+})
+
+sr.reveal('.carousel-inner', {scale: 1.1})
+
+sr.reveal('.wlcome, .featured, .about__content, .footer')
+sr.reveal('.discover__content, .img__container', {origin: 'left'})
+sr.reveal('.discover__img, .contact__in', {origin: 'right'})
+sr.reveal('.details, .sery', {interval: 100})
+
+
+
+
